27611cec358cf5d693bc2b6b7894f8cbdc4f3fc9
Greg Fitzgerald <netzdamon@gmail.com>
2009-09-07 19:11:02 -0400

    move the include for helpers into command.rb, and add resume
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/commands.rb
+++ lib/athenry/commands.rb
@@ -1,5 +1,7 @@
 require "athenry/commands/setup"
 require "athenry/commands/build"
 require "athenry/commands/clean"
+require "athenry/commands/resume"
 
 include Athenry::State
+include Athenry::Helper
