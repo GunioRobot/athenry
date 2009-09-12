733a110e8cf59f812d0a174fdd56cf878072a0ad
Greg Fitzgerald <netzdamon@gmail.com>
2009-09-11 22:39:16 -0400

    some @see linkage
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/shell/aliases.rb
+++ lib/athenry/shell/aliases.rb
@@ -5,43 +5,53 @@ module Athenry
   # * {Athenry::Build}.
   # * {Athenry::Clean}.
   module ShellAliases
-    
+   
+    # @see Athenry::Setup#fetch
     def fetch 
       Athenry::Execute::setup.fetch
     end
 
+    # @see Athenry::Setup#extract
     def extract
       Athenry::Execute::setup.extract
     end
 
+    # @see Athenry::Setup#snapshot
     def snapshot
       Athenry::Execute::setup.snapshot
     end
 
+    # @see Athenry::Setup#copy_scripts
     def copy_scripts
       Athenry::Execute::setup.copy_scripts
     end
 
+    # @see Athenry::Setup#copy_configs
     def copy_configs
       Athenry::Execute::setup.copy_configs
     end
     
+    # @see Athenry::Setup#generate_bashscripts
     def generate_bashscripts 
       Athenry::Execute::setup.generate_bashscripts
     end
 
+    # @see Athenry::Build#mount
     def mount
       Athenry::Execute::build.mount
     end
 
+    # @see Athenry::Build#chroot
     def chroot 
       Athenry::Execute::build.chroot
     end
 
+    # @see Athenry::Clean#unmount
     def unmount
       Athenry::Execute::clean.unmount
     end
 
+    # @see Athenry::Resume#from
     def continue 
       Athenry::Execute::resume.from
     end
