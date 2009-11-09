9b33e334fad6fe61015519316cf89ec57232b323
Greg Fitzgerald <netzdamon@gmail.com>
2009-11-09 00:10:53 -0500

    check md5sum of files. Move stage extraction into the new stage method. This is renamed from fetch. Added md5sum and extract methods
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/commands/setup.rb
+++ lib/athenry/commands/setup.rb
@@ -5,38 +5,35 @@ module Athenry
       setup_environment
     end
    
-    # If stage file is not found it fetches one from #{CONFIG.stageurl}
+    # If stage file is not found it fetches one from #{CONFIG.stageurl}, then extracts the file.
     # @return [String]
-    def fetch
-      announcing 'Fetching stage file' do
+    def stage
+      announcing 'Fetching stage tarball' do
         if File.exists?("#{CONFIG.workdir}/#{CONFIG.stageurl}")
           send_to_log('Stage file already exists skipping fetch', 'info')
         else
-          cmd "wget -c #{CONFIG.stageurl} -O #{CONFIG.workdir}/#{CONFIG.stageurl}"
+          cmd "wget -nc -c #{CONFIG.stageurl} -P #{CONFIG.workdir}"
+          cmd "wget -c #{CONFIG.stageurl}.DIGESTS -P #{CONFIG.workdir}"
+          cmd "wget -c #{CONFIG.stageurl}.CONTENTS -P #{CONFIG.workdir}"
         end
+        md5sum(CONFIG.stageurl, 'DIGESTS')
+        extract(CONFIG.stageurl, "#{CONFIG.workdir}/#{CONFIG.chrootdir}")
       end
       send_to_state('setup', 'fetch')
-    end
-
-    # Extracts the downloaded stage tarball to #{CONFIG.chrootdir}
-    # @return [String]
-    def extract
-      announcing 'Extracting stage file' do
-          cmd "tar xvjpf #{CONFIG.workdir}/#{CONFIG.stageurl} -C #{CONFIG.workdir}/#{CONFIG.chrootdir}"
-        end
-      send_to_state('setup', 'extract')
     end
 
     # Fetches a portage snapshot and extracts it to #{CONFIG.chrootdir}/usr/
     # @return [String]
     def snapshot
-      announcing 'Fetching and extracting portage snapshot' do
+      announcing 'Fetching portage snapshot' do
         if File.exists?("#{CONFIG.workdir}/#{CONFIG.snapshoturl}")
           send_to_log('Portage snapshot already exists skipping fetch', 'info')
         else
-          cmd "wget -c #{CONFIG.snapshoturl} -O #{CONFIG.workdir}/#{CONFIG.snapshoturl}"
+          cmd "wget -c #{CONFIG.snapshoturl} -P #{CONFIG.workdir}"
+          cmd "wget -c #{CONFIG.snapshoturl}.md5sum -P #{CONFIG.workdir}"
         end
-        cmd "tar xvjpf #{CONFIG.workdir}/#{CONFIG.snapshoturl} -C #{CONFIG.workdir}/#{CONFIG.chrootdir}/usr"
+        md5sum(CONFIG.snapshoturl, 'md5sum')
+        extract(CONFIG.snapshoturl, "#{CONFIG.workdir}/#{CONFIG.chrootdir}/usr")
       end
       send_to_state('setup', 'snapshot')
     end
@@ -71,5 +68,34 @@ module Athenry
       end
       send_to_state('setup', 'copy_configs')
     end
+    
+    private
+
+    # Checks the md5sum of a file from a url. If the md5sum doesn't passes it exits immediately.
+    # @param url [String]
+    # @param digest [String]
+    # @example
+    #   md5sum('http://www.foobar.com/path/file.tar.bz2', 'md5sum') => md5sum -c file.tar.bz2.md5sum --status
+    # @return [String]
+    def md5sum(url, digest)
+      announcing "Checking md5sum of #{filename(url)}" do
+        Dir.chdir("#{CONFIG.workdir}") do
+          cmd "md5sum -c #{CONFIG.workdir}/#{filename(url)}.#{digest} --status"
+        end
+      end
+    end
+
+    # Extracts the filename from a url, and extracts it to the specified path.
+    # @param url [String]
+    # @param path [String]
+    # @example
+    #   extract('http://www.example.com/path/file.tar.bz2', '/var/tmp/athenry/stage5')
+    # @return [String]
+    def extract(url, path)
+      announcing "Extracting #{filename(url)}" do
+        cmd "tar xvjpf #{CONFIG.workdir}/#{filename(url)} -C #{path}"
+      end
+    end
+
   end
 end
