baf836a05de424d7e8714693fc87363100c47df7
Greg Fitzgerald <netzdamon@gmail.com>
2009-11-14 15:35:04 -0500

    loop through OVERLAYKEY AND OVERLAYVAL arrays, then use playman or layman to install overlays based upon the users set package manager
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- scripts/athenry/modules/install_overlays.sh
+++ scripts/athenry/modules/install_overlays.sh
@@ -1,5 +1,32 @@
-if [ ${OVERLAYS} ]; then
-    bootstrap_overlays
+if [ ${OVERLAYKEY} ]; then
+  
+    for (( x=0 ; x < ${#OVERLAYKEY[*]}; x++ )) 
+    do
+        case $PKG_MANAGER in 
+        paludis)
+            # execute first command
+            if [ -z ${OVERLAYVAL[$x]} ]; then
+                echo playman -a ${OVERLAYKEY[$x]}
+            else
+                echo playman --layman-url ${OVERLAYVAL[$x]} -a ${OVERLAYKEY[$x]}
+            fi 
+            # execute second command
+            mkdir -p /var/paludis/repositories/${OVERLAYKEY[$x]}/.cache/names/
+            echo paludis -s x-${OVERLAYKEY[$x]}
+      ;; 
+        emerge)
+            # execute first command
+            if [ -z ${OVERLAYVAL[$x]} ]; then
+                echo layman -a ${OVERLAYKEY[$x]}
+            else
+                echo layman -f -o ${OVERLAYVAL[$x]} -k -a ${OVERLAYKEY[$x]}
+            fi 
+            # execute second command
+            echo layman -s ${OVERLAYKEY}
+        ;; 
+    esac
+  done
+        
 fi
 
 #vim:set ft=sh ts=4 sw=4 noet:
