75668d69be655b5598b32006b636847a217a3a1a
Greg Fitzgerald <netzdamon@gmail.com>
2009-11-14 00:58:47 -0500

    remove some old modules
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- scripts/athenry/modules/custom.sh
+++
@@ -1,9 +0,0 @@
-trap ctrl_c INT
-update_everything
-update_configs
-fix_broken
-if [ ${SETS} ]; then
-    install_sets
-fi
-
-#vim:set ft=sh ts=4 sw=4 noet:
--- scripts/athenry/modules/repair.sh
+++
@@ -1,4 +0,0 @@
-setup_chroot
-/bin/bash
-
-#vim:set ft=sh ts=4 sw=4 noet:
