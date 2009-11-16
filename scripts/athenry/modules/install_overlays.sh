4b3f2aa538ff8476b601deed0adda71956fa8c0b
Greg Fitzgerald <netzdamon@gmail.com>
2009-11-15 20:47:19 -0500

    Major cleanup, add || die's every where
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- scripts/athenry/modules/install_overlays.sh
+++ scripts/athenry/modules/install_overlays.sh
@@ -6,27 +6,29 @@ if [ ${OVERLAYKEY} ]; then
         paludis)
             # execute first command
             if [ -z ${OVERLAYVAL[$x]} ]; then
-                playman -a ${OVERLAYKEY[$x]}
+                playman -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]}"
             else
-                playman --layman-url ${OVERLAYVAL[$x]} -a ${OVERLAYKEY[$x]}
+                playman --layman-url ${OVERLAYVAL[$x]} -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]} with provided data ${OVERLAYVAL[$x]}"
             fi 
             # execute second command
+            paludis -s x-${OVERLAYKEY[$x]} || die "Failed syncing x-${OVERLAYKEY[$x]}"
             mkdir -p /var/paludis/repositories/${OVERLAYKEY[$x]}/.cache/names/
-            paludis -s x-${OVERLAYKEY[$x]}
       ;; 
         emerge)
             # execute first command
             if [ -z ${OVERLAYVAL[$x]} ]; then
-                layman -a ${OVERLAYKEY[$x]}
+                layman -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]}"
             else
-                layman -f -o ${OVERLAYVAL[$x]} -k -a ${OVERLAYKEY[$x]}
+                layman -f -o ${OVERLAYVAL[$x]} -k -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]} with provided data ${OVERLAYVAL[$x]}"
             fi 
             # execute second command
-            layman -s ${OVERLAYKEY}
+            layman -s ${OVERLAYKEY} || die "Failed to sync ${OVERLAYKEY} overlay"
         ;; 
     esac
   done
         
 fi
+
+rebuild_cache || die "Failed to rebuild paluids cache after adding overlays"
 
 #vim:set ft=sh ts=4 sw=4 noet:
