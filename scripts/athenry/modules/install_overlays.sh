702c2722ae915f6bf4aa7d32e9324d93213705d1
Greg Fitzgerald <netzdamon@gmail.com>
2009-11-15 22:32:39 -0500

    make sure variables are quoted, sourceh layman/make.conf after adding overlays for emerge
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- scripts/athenry/modules/install_overlays.sh
+++ scripts/athenry/modules/install_overlays.sh
@@ -1,28 +1,29 @@
-if [ ${OVERLAYKEY} ]; then
+if [ "${OVERLAYKEY}" ]; then
   
-    for (( x=0 ; x < ${#OVERLAYKEY[*]}; x++ )) 
+    for (( x=0 ; x < "${#OVERLAYKEY[*]}"; x++ )) 
     do
-        case ${PKG_MANAGER} in 
+        case "${PKG_MANAGER}" in 
         paludis)
             # execute first command
-            if [ -z ${OVERLAYVAL[$x]} ]; then
-                playman -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]}"
+            if [ -z "${OVERLAYVAL[$x]}" ]; then
+                playman -a "${OVERLAYKEY[$x]}" || die "Failed adding overlay ${OVERLAYKEY[$x]}"
             else
-                playman --layman-url ${OVERLAYVAL[$x]} -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]} with provided data ${OVERLAYVAL[$x]}"
+                playman --layman-url "${OVERLAYVAL[$x]}" -a "${OVERLAYKEY[$x]}" || die "Failed adding overlay ${OVERLAYKEY[$x]} with provided data ${OVERLAYVAL[$x]}"
             fi 
             # execute second command
-            paludis -s x-${OVERLAYKEY[$x]} || die "Failed syncing x-${OVERLAYKEY[$x]}"
-            mkdir -p /var/paludis/repositories/${OVERLAYKEY[$x]}/.cache/names/
+            paludis -s x-"${OVERLAYKEY[$x]}" || die "Failed syncing x-${OVERLAYKEY[$x]}"
+            mkdir -p /var/paludis/repositories/"${OVERLAYKEY[$x]}"/.cache/names/
       ;; 
         emerge)
             # execute first command
             if [ -z ${OVERLAYVAL[$x]} ]; then
-                layman -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]}"
+                layman -f -a "${OVERLAYKEY[$x]}" || die "Failed adding overlay ${OVERLAYKEY[$x]}"
             else
-                layman -f -o ${OVERLAYVAL[$x]} -k -a ${OVERLAYKEY[$x]} || die "Failed adding overlay ${OVERLAYKEY[$x]} with provided data ${OVERLAYVAL[$x]}"
+                layman -f -o "${OVERLAYVAL[$x]}" -k -a "${OVERLAYKEY[$x]}" || die "Failed adding overlay ${OVERLAYKEY[$x]} with provided data ${OVERLAYVAL[$x]}"
             fi 
             # execute second command
-            layman -s ${OVERLAYKEY} || die "Failed to sync ${OVERLAYKEY} overlay"
+            layman -s "${OVERLAYKEY}" || die "Failed to sync ${OVERLAYKEY} overlay"
+            echo 'source "/usr/local/portage/layman/make.conf"' >> /etc/make.conf || die "Failed adding layman source to make.conf" 
         ;; 
     esac
   done
