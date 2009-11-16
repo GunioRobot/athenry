297fc8aceff1306820ccfe6376f62866adf0c36e
Greg Fitzgerald <netzdamon@gmail.com>
2009-11-15 20:44:53 -0500

    Make config.rb a module, this is still a terrible mess
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/config.rb
+++ lib/athenry/config.rb
@@ -1,21 +1,28 @@
-CONFIG = configatron # global alias, shorter than typing configatron
+module Athenry
+  module Config
+      $shell_is_running = false
+      
+      # These are dynamic and are changed by Athenry::Helper#set_temp_options
+      CONFIG.nopaludis=false
+      CONFIG.freshen=false
+      # Options we didn't really want to leave up the user as of right now. 
+      CONFIG.logdir = 'log'
+      CONFIG.chrootdir = "chroot"
+      CONFIG.logfile = "#{CONFIG.chrootdir}.log"
+      CONFIG.statedir = '.athenry'
+      CONFIG.statefile = 'state'
+      CONFIG.scripts = "#{ATHENRY_ROOT}/scripts/"
+      CONFIG.homeconfig = "#{ENV['HOME']}/.config/athenry"
+      CONFIG.etcconfig = '/etc/athenry'
 
-# Options we didn't really want to leave up the user as of right now. 
-CONFIG.logdir = 'log'
-CONFIG.chrootdir = "chroot"
-CONFIG.logfile = "#{CONFIG.chrootdir}.log"
-CONFIG.statedir = '.athenry'
-CONFIG.statefile = 'state'
-CONFIG.scripts = "#{ATHENRY_ROOT}/scripts/"
-CONFIG.homeconfig = "#{ENV['HOME']}/.config/athenry/config.rb"
-CONFIG.etcconfig = '/etc/athenry/config.rb'
-
-if File.readable?("#{CONFIG.homeconfig}")
-  require "#{CONFIG.homeconfig}"
-  CONFIG.configs = "#{ENV['HOME']}/.config/athenry/etc/#{CONFIG.arch}"
-elsif File.readable?("#{CONFIG.etcconfig}")
-  require "#{CONFIG.etcconfig}"
-  CONFIG.configs = "/etc/athenry/etc/#{CONFIG.arch}"
-else
-  raise 'No configuration file present, please refer to doc/quickstart.markdown'
+      if File.readable?("#{CONFIG.homeconfig}/config.rb")
+        require "#{CONFIG.homeconfig}/config.rb"
+        CONFIG.configs = "#{CONFIG.homeconfig}/etc/#{CONFIG.arch}"
+      elsif File.readable?("#{CONFIG.etconfig}/config.rb")
+        require "#{CONFIG.etcconfig}/config.rb"
+        CONFIG.configs = "#{CONFIG.etcconfig}/etc/#{CONFIG.arch}"
+      else
+        raise 'No configuration file present, please refer to doc/quickstart.markdown'
+      end
+  end
 end
