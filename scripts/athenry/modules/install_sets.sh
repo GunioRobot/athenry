f71865065b2e6bf096182b9d38a9c846ec5fe90d
Greg Fitzgerald <netzdamon@gmail.com>
2009-11-15 20:46:12 -0500

    Make sets actually work with emerge
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- scripts/athenry/modules/install_sets.sh
+++ scripts/athenry/modules/install_sets.sh
@@ -1,6 +1,16 @@
 for _set in ${SETS[*]}
 do
-    ${PKG_INSTALL} "${_set}" || die "Failed installing set: ${_set}"
+    case ${PKG_MANAGER} in
+    paludis)
+        ${PKG_INSTALL} "${_set}" || die "Failed installing set: ${_set}"
+        ;;
+    emerge)
+        ${PKG_INSTALL} "@${_set}" || die "Failed installing set: @${_set}"
+        ;;
+    *)
+        die "${PKG_MANAGER} is invalid please see the quickstart guide."
+        ;;
+    esac
 done
 
 #vim:set ft=sh ts=4 sw=4 noet:
