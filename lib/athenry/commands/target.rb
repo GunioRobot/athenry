f7eab8f2f6a2d196e2b93b30cc2ee62c09613c99
Greg Fitzgerald <netzdamon@gmail.com>
2010-01-31 19:08:07 -0500

    switch to new target method
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/commands/target.rb
+++ lib/athenry/commands/target.rb
@@ -2,40 +2,30 @@ module Athenry
   class Target 
 
     def initialize
-      must_be_root
-      check_for_setup(:run)
-      update_chroot
     end
 
-    def build(args)
+    def build(*args)
       args.each { |cmd| send(cmd) }
     end
 
     # Steps required to build a stage3
-    # @see Athenry::Execute::build
+    # @see Athenry::build
     # @return [String]
     def stage3
       set_temp_options(:nopaludis => true) do
-        Athenry::Execute::build.install_pkgmgr
-        Athenry::Execute::build.sync
-        Athenry::Execute::build.update_everything
-        Athenry::Execute::build.update_configs
-        Athenry::Execute::build.rebuild
+        Athenry::build.target("install_pkgmgr", "sync", "update_everything", "update_configs", "rebuild")
       end
     end
 
     # Steps required to build a custom stage
-    # @see Athenry::Execute::build
+    # @see Athenry::build
     # @return [String]
     def custom
-      Athenry::Execute::build.install_pkgmgr
-      Athenry::Execute::build.sync
-      Athenry::Execute::build.update_everything
-      Athenry::Execute::build.rebuild
-      Athenry::Execute::build.update_configs
-      Athenry::Execute::build.install_overlays
-      Athenry::Execute::build.install_sets
-      Athenry::Execute::build.rebuild
+      Athenry::build.target("install_pkmgr", "sync", "update_everything", "rebuild", "update_configs", "install_overlays", "install_sets", "rebuild")
+    end
+
+    def setup
+      Athenry::setup.target("fetchstage", "extractstage", "fetchsnapshot", "updatesnapshot", "copysnapshot", "generate_bashscripts", "copy_scripts", "copy_configs")
     end
 
   end
