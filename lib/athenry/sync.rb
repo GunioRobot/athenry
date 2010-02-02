e88080e68053538acf1aebee3c1a4fbdbfe16bf2
Greg Fitzgerald <netzdamon@gmail.com>
2010-02-02 00:34:17 -0500

    sync class, small rsync wrapper
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

---
+++ lib/athenry/sync.rb
@@ -0,0 +1,16 @@
+module Athenry
+  class Sync 
+
+    attr_accessor :uri, :output, :options
+
+    def initialize(opts={})
+      self.uri = opts[:uri]
+      self.output = opts[:output]
+      self.options = opts[:options] || "-apv --delete"
+    end
+    
+    def files
+      cmd "rsync #{options} #{uri} #{output}"
+    end
+  end
+end
