8c4f82eac18afd1a594ee9f97c7e691fefed258e
Greg Fitzgerald <netzdamon@gmail.com>
2010-02-06 22:50:11 -0500

    fix resume support
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/resumetree.rb
+++ lib/athenry/resumetree.rb
@@ -1,18 +1,22 @@
 module Athenry
   RESUMETREE = {
     'setup' => {
-      '1' => [ 'snapshot', 'generate_bashscripts', 'copy_scripts', 'copy_configs' ],
-      '2' => [ 'generate_bashscripts', 'copy_scripts', 'copy_configs' ],
-      '3' => [ 'copy_scripts', 'copy_configs' ],
-      '4' => [ 'copy_configs' ],
+      '1' => [ 'extractstage', 'fetchsnapshot', 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      '2' => [ 'fetchsnapshot', 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      '3' => [ 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      '4' => [ 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      '5' => [ 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      '6' => [ 'copy_scripts', 'copy_configs'  ],
+      '7' => [ 'copy_configs'  ],
     },
     'build' => {
-      '1' => [ 'install_pkgmgr', 'update_everything', 'etc_update', 'install_overlays', 'install_sets', 'rebuild'  ],
-      '2' => [ 'update_everything', 'etc_update', 'install_overlays', 'install_sets', 'rebuild'  ],
-      '3' => [ 'etc_update', 'install_overlays', 'install_sets', 'rebuild'  ],
-      '4' => [ 'install_overlays', 'install_sets', 'rebuild'  ],
-      '5' => [ 'install_sets', 'rebuild'  ],
-      '6' => [ 'rebuild'  ],
+      '1' => [ 'sync', 'install_pkgmgr', 'update_everything', 'etc_update', 'install_overlays', 'install_sets', 'rebuild'  ],
+      '2' => [ 'install_pkgmgr', 'update_everything', 'etc_update', 'install_overlays', 'install_sets', 'rebuild'  ],
+      '3' => [ 'update_everything', 'etc_update', 'install_overlays', 'install_sets', 'rebuild'  ],
+      '4' => [ 'etc_update', 'install_overlays', 'install_sets', 'rebuild'  ],
+      '5' => [ 'install_overlays', 'install_sets', 'rebuild'  ],
+      '6' => [ 'install_sets', 'rebuild'  ],
+      '7' => [ 'rebuild'  ],
     },
   }
 end
