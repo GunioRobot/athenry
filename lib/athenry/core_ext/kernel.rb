1ae9aee0cfaaf3e1d571650329c31cc2f3da3eb3
Greg Fitzgerald <netzdamon@gmail.com>
2010-02-09 21:57:45 -0500

    fix formating
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/core_ext/kernel.rb
+++ lib/athenry/core_ext/kernel.rb
@@ -10,7 +10,7 @@ module Kernel
 
     def parse_caller(at)
         if /^(.+?):(\d+)(?::in `(.*)')?/ =~ at
-            file = Regexp.last_match[1]
+        file = Regexp.last_match[1]
         line = Regexp.last_match[2].to_i
         method = Regexp.last_match[3]
         [file, line, method]
