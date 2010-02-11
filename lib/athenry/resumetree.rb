81437c134c4cf4a1362faa5ebddf7a47d4465d13
Greg Fitzgerald <netzdamon@gmail.com>
2010-02-10 23:51:01 -0500

    don't quote and use symbols
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/resumetree.rb
+++ lib/athenry/resumetree.rb
@@ -1,26 +1,26 @@
 module Athenry
   RESUMETREE = {
-    'setup' => {
-      '1' => [ 'extractstage', 'fetchsnapshot', 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
-      '2' => [ 'fetchsnapshot', 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
-      '3' => [ 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
-      '4' => [ 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
-      '5' => [ 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
-      '6' => [ 'copy_scripts', 'copy_configs'  ],
-      '7' => [ 'copy_configs'  ],
+    :setup => {
+      1 => [ 'extractstage', 'fetchsnapshot', 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      2 => [ 'fetchsnapshot', 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      3 => [ 'extractsanpshot', 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      4 => [ 'updatesnapshot', 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      5 => [ 'copysnapshot', 'copy_scripts', 'copy_configs'  ],
+      6 => [ 'copy_scripts', 'copy_configs'  ],
+      7 => [ 'copy_configs'  ],
     },
-    'custom' => {
-      '1' => [ 'update_everything', 'rebuild', 'etc_update', 'install_overlays', 'install_sets', 'rebuild' ],
-      '2' => [ 'rebuild', 'etc_update', 'install_overlays', 'install_sets', 'rebuild' ],
-      '3' => [ 'etc_update', 'install_overlays', 'install_sets', 'rebuild' ],
-      '4' => [ 'install_overlays', 'install_sets', 'rebuild' ],
-      '5' => [ 'install_sets', 'rebuild' ],
-      '6' => [ 'rebuild' ],
+    :custom => {
+      1 => [ 'update_everything', 'rebuild', 'etc_update', 'install_overlays', 'install_sets', 'rebuild' ],
+      2 => [ 'rebuild', 'etc_update', 'install_overlays', 'install_sets', 'rebuild' ],
+      3 => [ 'etc_update', 'install_overlays', 'install_sets', 'rebuild' ],
+      4 => [ 'install_overlays', 'install_sets', 'rebuild' ],
+      5 => [ 'install_sets', 'rebuild' ],
+      6 => [ 'rebuild' ],
     },
-    'stage3' => {
-      '1' => [ 'update_everything', 'etc_update', 'rebuild' ], 
-      '2' => [ 'etc_update', 'rebuild' ], 
-      '3' => [ 'rebuild' ], 
+    :stage3 => {
+      1 => [ 'update_everything', 'etc_update', 'rebuild' ], 
+      2 => [ 'etc_update', 'rebuild' ], 
+      3 => [ 'rebuild' ], 
     },
   }
 end
