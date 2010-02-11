131108c8593a8f23a29edbbb13342c537b686c2c
Greg Fitzgerald <netzdamon@gmail.com>
2010-02-10 22:10:35 -0500

    no longer need to load ShellAliases. Load Athenry::Config before the helpers
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/commands.rb
+++ lib/athenry/commands.rb
@@ -2,10 +2,9 @@ require 'athenry/commands/setup'
 require 'athenry/commands/build'
 require 'athenry/commands/target'
 require 'athenry/commands/clean'
-require 'athenry/shell/aliases'
 require 'athenry/commands/resume'
 require 'athenry/commands/rescue'
 require 'athenry/commands/info'
 
-include Athenry::Helper
 include Athenry::Config
+include Athenry::Helper
