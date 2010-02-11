d7344f7bea6edb325efa77ca7954a5161bed3f24
Greg Fitzgerald <netzdamon@gmail.com>
2010-02-11 14:25:15 -0500

    display some useful information when we rescue from Gem::LoadError
    Signed-off-by: Greg Fitzgerald <netzdamon@gmail.com>

--- lib/athenry/depends.rb
+++ lib/athenry/depends.rb
@@ -12,10 +12,16 @@ begin
   gem 'rconfig', '>= 0.3.2'
   require 'rconfig'
 rescue Gem::LoadError
+  $stdout.puts "Please install the rconfig gem by running the following:"
+  $stdout.puts "gem install rconfig"
+  exit 1
 end
 
 begin
   gem 'progressbar', '>= 0.9.0'
   require 'progressbar'
 rescue Gem::LoadError
+  $stdout.puts "Please install the progressbar gem by running the following:"
+  $stdout.puts "gem install progressbar"
+  exit 1
 end
